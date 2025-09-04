038027100c05734d64580ddc5e8b026b72434d7c
ray_rid <rid843@mail.ru>
2025-09-04 17:57:21 +0700

    Added WeaponFactory and created weapon configs

---
+++ Assets/Scripts/Factories/WeaponFactory.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using Domain.Core;
+
+namespace Factories
+{
+    public static class WeaponFactory
+    {
+        public static Weapon FromConfig(WeaponConfig config)
+        {
+            if (config == null)
+                throw new System.ArgumentNullException(nameof(config));
+            
+            return new Weapon(
+                config.Name,
+                config.BaseDamage,
+                config.Type
+            );
+        }
+    }
+}
